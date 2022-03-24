238f895312acbd555679beee9b6c44c7acbee8f1
Vinicius Silva Pereira <viniciussilvapereira24@gmail.com>
2022-03-24 01:25:10 -0300

    Adding use case for 'People'

--- cineclube/src/main/java/br/com/cineclube/model/Person.java
+++ cineclube/src/main/java/br/com/cineclube/model/Person.java
@@ -5,36 +5,41 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 public class Person {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
-	
+
+	@NotBlank
 	private String name;
-	
+
 	public Person() {
-		
 	}
+
 	public Person(String name) {
 		this.name = name;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
-	
 }
